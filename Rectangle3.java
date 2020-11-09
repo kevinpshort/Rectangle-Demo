1a8d397dfed2e8b66ffbd2c4b9eb33bbdefd3139
kevinpshort <69334576+kevinpshort@users.noreply.github.com>
2020-11-08 21:54:58 -0600

    Create Rectangle3.java

---
+++ Rectangle3.java
@@ -0,0 +1,22 @@
+package rectanglePkg;
+
+public class Rectangle3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double width;
+		double height;
+		double area;
+		double perimeter;
+		
+		width = 2;
+		height = 3;
+		area = width * height;
+		perimeter = (2 * width) + (2 * height);
+		
+		System.out.println("W: " + width + "  H: " + height + "  A: " + area + "  P: " +  perimeter);
+
+	}
+
+}
