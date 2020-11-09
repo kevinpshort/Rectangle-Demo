5f6985f265f20b70239fb1ba34cbeb34b6096503
kevinpshort <69334576+kevinpshort@users.noreply.github.com>
2020-11-08 21:43:31 -0600

    A set of Rectangle .java prrograms
    This a set of rectangle files that are progressively changed using GitHub

---
+++ Rectangle1.java
@@ -0,0 +1,20 @@
+package rectanglePkg;
+
+public class Rectangle1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double width;
+		double height;
+		double area;
+		
+		width = 2;
+		height = 3;
+		area = width * height;
+		
+		System.out.println("W: " + width + "  H: " + height + "  A: " + area);
+
+	}
+
+}
